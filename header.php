f5d49924bc58a6636f81d7d8fad60df67974d35f
betodasilva <jrs_robert@hotmail.com>
2018-04-04 00:18:10 -0300

    Add search widget to header

--- header.php
+++ header.php
@@ -27,15 +27,27 @@
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'sindcon' ); ?></a>
 
 	<header id="masthead" class="site-header">
-
+		<div class="header__top hide-sm">
+			<div class="container">
+				<div class="row gutters">
+					<div class="col col-4 offset-8 text-right">
+						<a href="https://www.google.com/maps/place/SINDCON/@-8.0570922,-34.8879328,17z/data=!3m1!4b1!4m5!3m4!1s0x7ab18c017b239af:0x368b3e530acc1932!8m2!3d-8.0570975!4d-34.8857441" target="_blank" class="no-decoration">
+							<i class="material-icons midgray v-align-middle">map</i> Ver no mapa
+						</a>
+						<span style="margin-left:10px;margin-right:8px;" class="no-decoration"><i class="material-icons midgray v-align-middle">phone</i> 81 3423.8149</span>
+					</div>
+				</div>
+			</div>
+		</div>
 		<div class="container">
-			<div class="flex-header">
+			
+			<div class="flex-header align-middle">
 				<div class="site-branding">
 						<?php the_custom_logo(); ?>
 				</div><!-- .site-branding -->	
 				
-				<div class="toggle-container show-sm text-right">
-					<a href="#" data-component="toggleme" data-target="#site-navigation" class="button outline"><i class="kube-menu"></i>	MENU</a>
+				<div class="toggle-container text-right">
+					<a href="#" data-component="toggleme" data-target="#site-navigation" class="button outline green"><i class="kube-menu"></i>	MENU</a>
 				</div>
 				<div class="col col-6 push-right hide-sm">
 					<div class="site-description">
@@ -44,8 +56,11 @@
 							echo $the_description;
 						?>
 					</div>
-					
+					<?php if ( is_active_sidebar( 'header-widget' ) ) echo '<div class="header-widget">'; dynamic_sidebar( 'header-widget' ); echo '</div>'; ?>
 				</div>
+			</div>
+			<div class="col col-12 show-sm">
+				<?php if ( is_active_sidebar( 'header-widget' ) ) echo '<div class="header-widget">'; dynamic_sidebar( 'header-widget' ); echo '</div>'; ?>
 			</div>
 		</div>
 		<nav id="site-navigation" class="main-navigation hide-sm">
