fea742f3af633ebcbc65b2f6e87869111e875da5
betodasilva <jrs_robert@hotmail.com>
2018-04-04 00:18:19 -0300

    Improve search results

--- search.php
+++ search.php
@@ -12,44 +12,52 @@ get_header();
 
 	<section id="primary" class="content-area">
 		<main id="main" class="site-main">
+			<div class="container custom-container">
+				<div class="row gutters">
+					<div class="col col-8 col-posts">
+						<?php if ( have_posts() ) : ?>
 
-		<?php if ( have_posts() ) : ?>
+							<header class="page-header">
+								<h1 class="page-title">
+									<?php
+									/* translators: %s: search query. */
+									printf( esc_html__( 'Search Results for: %s', 'sindcon' ), '<span>' . get_search_query() . '</span>' );
+									?>
+								</h1>
+							</header><!-- .page-header -->
 
-			<header class="page-header">
-				<h1 class="page-title">
-					<?php
-					/* translators: %s: search query. */
-					printf( esc_html__( 'Search Results for: %s', 'sindcon' ), '<span>' . get_search_query() . '</span>' );
-					?>
-				</h1>
-			</header><!-- .page-header -->
+						<div class="row gutters">
+							<?php
+							/* Start the Loop */
+							while ( have_posts() ) :
+								the_post(); ?>
 
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
+								<div class="col col-6">
+									<?php get_template_part( 'template-parts/content', get_post_type() ); ?>
+								</div>
+							<?php
 
-				/**
-				 * Run the loop for the search to output the results.
-				 * If you want to overload this in a child theme then include a file
-				 * called content-search.php and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', 'search' );
+							endwhile;?>
+						</div>
+						<?php
+							else : ?>
+								<div class="col col-12">
+									<?php get_template_part( 'template-parts/content', 'none' ); ?>
+								</div>
+							<?php
 
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
-		?>
-
+							endif;
+							?>
+						
+					</div><!-- .col-8  -->
+					<div class="col col-4">
+						<?php get_sidebar(); ?>
+					</div>
+				</div>
+				
+			</div>
 		</main><!-- #main -->
 	</section><!-- #primary -->
 
 <?php
-get_sidebar();
 get_footer();
